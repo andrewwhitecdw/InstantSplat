6194644bc77c45201bb61b9baab08191e2dc9944
Andrew White <andrewh@cdw.com>
2026-07-27 20:48:10 -0500

    fix: load_images passes float crop box to PIL

--- dust3r/utils/image.py
+++ dust3r/utils/image.py
@@ -111,7 +111,7 @@ def load_images(folder_or_list, size, square_ok=False, verbose=True):
         else:
             halfw, halfh = ((2*cx)//16)*8, ((2*cy)//16)*8
             if not (square_ok) and W == H:
-                halfh = 3*halfw/4
+                halfh = (3*halfw)//4
             img = img.crop((cx-halfw, cy-halfh, cx+halfw, cy+halfh))
 
         W2, H2 = img.size
